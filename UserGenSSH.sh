8a662a6228c884adba0b384c25c21b4bd8e232db
christopherGitHub01 <christopher.wargo@mymail.champlain.edu>
2021-03-08 04:53:46 +0000

    New UserGenSSH.sh Script

---
+++ UserGenSSH.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1
+sudo mkdir /home/$1/.ssh
+sudo cp public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
